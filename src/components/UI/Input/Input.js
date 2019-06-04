4e1f00676e9725cef1fd060944609f07eeecac9e
Xavier Carrera Gimbert <xalsar@protonmail.com>
2019-06-04 10:55:16 +0200

    267. Handle user input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -6,17 +6,17 @@ const input = (props) => {
 
     switch (props.elementType) {
         case ('input'):
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} />
+            inputElement = <input className={classes.InputElement} onChange={props.changed} {...props.elementConfig} value={props.value} />
             break
         case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} />
+            inputElement = <textarea className={classes.InputElement} onChange={props.changed} {...props.elementConfig} value={props.value} />
             break
         case ('select'):
             inputElement = (
                 <select
-                    className={classes.InputElement}
+                    className={classes.InputElement} onChange={props.changed}
                     value={props.value}>
-                    {props.elementConfig.options.map((option)=>(
+                    {props.elementConfig.options.map((option) => (
                         <option key={option.value} value={option.value}>
                             {option.displayValue}
                         </option>
@@ -25,7 +25,7 @@ const input = (props) => {
             )
             break
         default:
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} />
+            inputElement = <input className={classes.InputElement} onChange={props.changed} {...props.elementConfig} value={props.value} />
     }
 
     return (
