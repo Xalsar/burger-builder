6bef184478424b886ba304c38c900dd4fef15a00
Xavier Carrera Gimbert <xalsar@protonmail.com>
2019-06-04 20:13:46 +0200

    275. Handle overall form validation

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,18 +3,22 @@ import classes from './Input.module.css'
 
 const input = (props) => {
     let inputElement = null
+    const inputClasses = [classes.InputElement]
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid)
+    }
 
     switch (props.elementType) {
         case ('input'):
-            inputElement = <input className={classes.InputElement} onChange={props.changed} {...props.elementConfig} value={props.value} />
+            inputElement = <input className={inputClasses.join(' ')} onChange={props.changed} {...props.elementConfig} value={props.value} />
             break
         case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} onChange={props.changed} {...props.elementConfig} value={props.value} />
+            inputElement = <textarea className={inputClasses.join(' ')} onChange={props.changed} {...props.elementConfig} value={props.value} />
             break
         case ('select'):
             inputElement = (
                 <select
-                    className={classes.InputElement} onChange={props.changed}
+                    className={inputClasses.join(' ')} onChange={props.changed}
                     value={props.value}>
                     {props.elementConfig.options.map((option) => (
                         <option key={option.value} value={option.value}>
@@ -25,7 +29,7 @@ const input = (props) => {
             )
             break
         default:
-            inputElement = <input className={classes.InputElement} onChange={props.changed} {...props.elementConfig} value={props.value} />
+            inputElement = <input className={inputClasses.join(' ')} onChange={props.changed} {...props.elementConfig} value={props.value} />
     }
 
     return (
